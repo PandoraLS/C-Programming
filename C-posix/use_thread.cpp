60dd261a2e9c46f80f5f2ed16e8daa661762f7e3
GuiLS <lisen_work@163.com>
2020-11-20 11:10:35 +0800

    c pthread

--- C-posix/use_thread.cpp
+++ C-posix/use_thread.cpp
@@ -1,43 +1,35 @@
 /*
  * @Author: seenli 
- * @Date: 2020-11-19 20:51:30 
+ * @Date: 2020-11-20 10:41:25 
  * @Last Modified by: seenli
- * @Last Modified time: 2020-11-19 20:51:59
+ * @Last Modified time: 2020-11-20 11:09:40
  */
 
 /**
  * 参考链接：https://www.runoob.com/cplusplus/cpp-multithreading.html
- * 
 */
 
-#include <iostream>
-// 必须的头文件
-#include <pthread.h>
- 
-using namespace std;
- 
-#define NUM_THREADS 5
- 
-// 线程的运行函数
-void* say_hello(void* args)
-{
-    cout << "Hello Runoob！" << endl;
-    return 0;
-}
- 
-int main()
-{
-    // 定义线程的 id 变量，多个变量使用数组
-    pthread_t tids[NUM_THREADS];
-    for(int i = 0; i < NUM_THREADS; ++i)
-    {
-        //参数依次是：创建的线程id，线程参数，调用的函数，传入的函数参数
-        int ret = pthread_create(&tids[i], NULL, say_hello, NULL);
-        if (ret != 0)
-        {
-           cout << "pthread_create error: error_code=" << ret << endl;
-        }
-    }
-    //等各个线程退出后，进程才结束，否则进程强制结束了，线程可能还没反应过来；
-    pthread_exit(NULL);
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h>  //Header file for sleep(). man 3 sleep for details. 
+#include <pthread.h> 
+// A normal C function that is executed as a thread  
+// when its name is specified in pthread_create() 
+void *myThreadFun(void *vargp) 
+{ 
+    sleep(1); 
+    printf("Printing GeeksQuiz from Thread \n"); 
+    return NULL; 
+} 
+   
+int main() 
+{ 
+    pthread_t thread_id; 
+    printf("Before Thread\n"); 
+    pthread_create(&thread_id, NULL, myThreadFun, NULL); 
+    pthread_join(thread_id, NULL); 
+    printf("After Thread\n"); 
+
+    system("pause");
+    exit(0); 
 }
