8161ba221a0902cde57873292263f2d0e8fbef9e
sen <lisen_work@163.com>
2021-01-03 23:13:50 +0800

    work for ch08

--- Ch08/ch08_drill1/use.cpp
+++ Ch08/ch08_drill1/use.cpp
@@ -2,15 +2,17 @@
  * @Author: seenli
  * @Date: 2021-01-03 20:58:40
  * @LastEditors: seenli
- * @LastEditTime: 2021-01-03 21:18:53
+ * @LastEditTime: 2021-01-03 23:00:10
  * @FilePath: \Ch08\use.cpp
  * @Description: Programming Principles and Practice Using C++ Second Edition
  */
 
 #include "my.h"
+#include <stdlib.h>
 
 int main()
 {
     cout << m1 << "+" << m2 << "=" << sum(m1, m2) << endl;
+    system("pause");
     return 0;
 }
